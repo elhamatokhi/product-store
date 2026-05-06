3a0cce37aab78a474459025be37b8f4830591372
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-05-06 10:53:42 +0200

    React query setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { SettingsProvider } from './context/SettingsContext.jsx'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient = new QueryClient()
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </QueryClientProvider>
+  </Provider>
 )
